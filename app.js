7c8ece53a17c2ef1c3a2036f949c4253499e9ba7
HelloWorldLau <116593761+HelloWorldLau@users.noreply.github.com>
2022-11-07 19:32:09 -0500

    commit inital

---
+++ app.js
@@ -0,0 +1,15 @@
+function tolike(id, dislike){
+    
+    document.getElementById(id).style.color = "black";
+    document.getElementById(dislike).style.display = "none";
+    
+    
+}
+
+function toDislike(id, like){
+    
+    document.getElementById(id).style.color = "black";
+    document.getElementById(like).style.display = "none";
+    
+    
+}
